0ad73109308780eeee986c99f425c098b6125268
BENJAMIN RIGSBY <rigsby@nike.cs.uga.edu>
2016-04-16 20:44:10 -0400

    updated math interative/recursive

--- src/main/java/cs1302/calc/RecursiveMath.java
+++ src/main/java/cs1302/calc/RecursiveMath.java
@@ -12,35 +12,59 @@ package cs1302.calc;
 public class RecursiveMath implements Math {
 
     public int inc(int n) {
-        throw new UnsupportedOperationException();
+        return n + 1;
     } // inc
 
     public int dec(int n) {
-        throw new UnsupportedOperationException();
+        return n - 1;
     } // dec
 
     public int add(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+	if(rhs == 0) return lhs;
+        return add(inc(lhs), dec(rhs));
     } // add
 
     public int sub(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        if(rhs == 0) return lhs;
+	if(lhs == 0) return 0;
+	return sub(dec(lhs), dec(rhs));
     } // sub
 
     public int mul(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        return mul_rec(lhs, rhs, 0);
     } // mul
 
+    private int mul_rec(int lhs, int rhs, int product) {
+	if(rhs == 0) return product;
+        return mul_rec(lhs, dec(rhs), add(product, lhs));
+    } // mul_rec
+
     public int div(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
-    } // div
+	return div_rec(lhs, rhs, 0);
+    }//div
+
+    private int div_rec(int lhs, int rhs, int sum) {
+	if(rhs < lhs) return sum;
+	return div_rec(sub(lhs, rhs), rhs, inc(sum));
+    }//div_rec
 
     public int fac(int n) {
-        throw new UnsupportedOperationException();
+	if(n == 0) return 0;
+        return fac_rec(n, 1);
     } // fac
 
+    public int fac_rec(int n, int sum) {
+	if(n == 0) return sum;
+	return fac_rec(dec(n), mul(sum, n));
+    } // fac_rec
+
     public int pow(int lhs, int rhs) {
-        throw new UnsupportedOperationException();
+        return pow_rec(lhs, rhs, 1);
     } // pow
+
+    public int pow_rec(int lhs, int rhs, int product) {
+	if(rhs == 0) return product;
+        return pow_rec(lhs, dec(rhs), mul(product, lhs));
+    } // pow_rec
 
 } // RecursiveMath
